70ec15319ae0366afaeb4ad6109a7a9499371ba3
YIMHYEONGSUP <gud1313@naver.com>
2022-10-18 19:07:50 +0900

    2022 10 18 tue 7:07pm

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <TodoHeader/>
+    <TodoInput/>
+    <TodoList/>
+    <TodoFooter/>
+  </div>
+</template>
+
+<script>
+
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+//  helper function 
+
+
+
+export default {
+  data(){ // state
+    return {
+     
+    }
+  },
+  
+  methods: { // mutations
+
+  },
+  components: { 
+    TodoHeader,
+    TodoFooter,
+    TodoInput,
+    TodoList,
+  }
+}
+</script>
+
+<style>
+body{
+  text-align: center;
+  background-color: #f6f6f6;
+}
+input{
+  border-style: groove;
+  width: 200px;
+}
+button{
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0,0,0, 0.03);
+}
+</style>
